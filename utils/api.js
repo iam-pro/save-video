e4e839c51d942fd7a17ed2e057aad8efb98763cd
iam-pro <moigugul0@gmail.com>
2021-02-26 08:59:41 +0000

    Initial commit

---
+++ utils/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+// const apiUrl = 'https://save-video.vercel.app/api';
+const apiUrl = 'https://save-video.herokuapp.com/api';
+// const apiUrl = 'http://localhost:3000/api';
+
+const api = axios.create({
+    baseURL: `${apiUrl}`,
+    timeout: 30000,
+    headers: {'X-Custom-Header': 'Ytdl'}
+});
+
+export default api;
+
+// https://www.youtube.com/watch?v=HagVnWAeGcM - not playing
